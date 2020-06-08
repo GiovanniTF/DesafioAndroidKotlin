e683e7b23cdc7faf0af4c428d87707bd88d4789c
giovannitf <Gi030201>
2020-06-08 12:28:49 -0300

    Implementa PullrequestPosts

---
+++ app/src/main/java/br/com/giovanni/desafioandroidkotlinapp/api/PullRequestPosts.kt
@@ -0,0 +1,44 @@
+package br.com.giovanni.desafioandroidkotlinapp.api
+
+import com.google.gson.annotations.SerializedName
+
+data class PullRequestPosts(
+
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("body")
+    val body: String,
+
+    @SerializedName("user")
+    val user: User,
+
+    @SerializedName("head")
+    val head: Head
+
+)
+
+data class User(
+    @SerializedName("avatar_url")
+    var avatar_url: String,
+
+    @SerializedName("login")
+    var login: String
+
+)
+
+data class Head(
+    @SerializedName("repo")
+    val repo: Repo
+)
+
+data class Repo(
+    @SerializedName("description")
+    val description: String,
+
+    @SerializedName("full_name")
+    val full_name: String
+)
