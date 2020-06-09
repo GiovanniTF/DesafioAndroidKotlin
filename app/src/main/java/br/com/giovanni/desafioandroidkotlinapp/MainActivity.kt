bbb2ca3f93b5a9b3eeb5fe55a5c07359b68cb52d
giovannitf <Gi030201>
2020-06-09 16:41:06 -0300

    Implementa SetSupportActionBar altera build.gradle

--- app/src/main/java/br/com/giovanni/desafioandroidkotlinapp/MainActivity.kt
+++ app/src/main/java/br/com/giovanni/desafioandroidkotlinapp/MainActivity.kt
@@ -2,11 +2,41 @@ package br.com.giovanni.desafioandroidkotlinapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar_id))
+
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.repoFragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBar(navController, appBarConfiguration)
+
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.repoFragment).navigateUp(appBarConfiguration)
     }
 }
+
