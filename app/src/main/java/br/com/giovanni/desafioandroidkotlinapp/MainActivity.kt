16aabb3752e338bb294d1045e6a998a71479510b
giovannitf <Gi030201>
2020-06-18 21:46:15 -0300

    Insere Toolbar, adiciona cores e style upButton

--- app/src/main/java/br/com/giovanni/desafioandroidkotlinapp/MainActivity.kt
+++ app/src/main/java/br/com/giovanni/desafioandroidkotlinapp/MainActivity.kt
@@ -2,41 +2,27 @@ package br.com.giovanni.desafioandroidkotlinapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navHost: NavHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.main_fragment_id) as NavHostFragment
+    }
+    private val navController get() = navHost.navController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(findViewById(R.id.toolbar_id))
+        setSupportActionBar(toolbar_id)
 
-        val host: NavHostFragment =
-            supportFragmentManager.findFragmentById(R.id.repoFragment) as NavHostFragment? ?: return
-
-        val navController = host.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        setupActionBar(navController, appBarConfiguration)
-
-    }
-
-    private fun setupActionBar(
-        navController: NavController,
-        appBarConfig: AppBarConfiguration
-    ) {
-        setupActionBarWithNavController(navController, appBarConfig)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.repoFragment).navigateUp(appBarConfiguration)
+        toolbar_id.setupWithNavController(navController, appBarConfiguration)
     }
 }
 
