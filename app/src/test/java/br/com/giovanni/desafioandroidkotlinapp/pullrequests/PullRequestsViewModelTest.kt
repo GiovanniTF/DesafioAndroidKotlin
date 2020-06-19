f28fec6214c34699b681dd96407451f1be9d3f4d
giovannitf <Gi030201>
2020-06-19 15:39:24 -0300

    Insere testes unitarios PullRequestsViewModel

--- app/src/test/java/br/com/giovanni/desafioandroidkotlinapp/pullrequests/PullRequestsViewModelTest.kt
+++ app/src/test/java/br/com/giovanni/desafioandroidkotlinapp/pullrequests/PullRequestsViewModelTest.kt
@@ -1,25 +1,32 @@
 package br.com.giovanni.desafioandroidkotlinapp.pullrequests
 
 import br.com.giovanni.desafioandroidkotlinapp.InstantExecutionRule
-import br.com.giovanni.desafioandroidkotlinapp.api.*
+import br.com.giovanni.desafioandroidkotlinapp.api.Head
+import br.com.giovanni.desafioandroidkotlinapp.api.PullRequestPosts
+import br.com.giovanni.desafioandroidkotlinapp.api.Repo
+import br.com.giovanni.desafioandroidkotlinapp.api.User
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import retrofit2.Response
+import java.io.IOException
 
 class PullRequestsViewModelTest {
 
     @ExperimentalCoroutinesApi
     @get:Rule
     val instantExecutionRule = InstantExecutionRule()
-
     private val interactor = mockk<GetPullRequestInteractor>()
+    private val userMock = "CyC2018"
+    private val repositoryMock = "CS-Notes"
+    private val viewModel = PullRequestViewModel(interactor)
 
-    private val page: Int = 1
-
-    private fun createApiResponse() = ApiResponse(
+    private fun createApiResponse() =
         listOf(
             PullRequestPosts(
                 4,
@@ -35,12 +42,63 @@ class PullRequestsViewModelTest {
                 )
             )
         )
-    )
+
 
     @Test
-    fun callsPullRequestState() {
+    fun callsPullRequestsState() {
         val apiResponse = createApiResponse()
 
-        coEvery { interactor.execute("CyC2018", "CS-Notes") } returns Response.success()
+        coEvery { interactor.execute(userMock, repositoryMock) } returns Response.success(
+            apiResponse
+        )
+
+        viewModel.getPullRequest(userMock, repositoryMock)
+
+        assertEquals(
+            viewModel.getPullRequestViewState().value,
+            PullRequestViewState.Request(apiResponse)
+        )
+    }
+
+    @Test
+    fun callsPullRequests_EmptyState() {
+        coEvery { interactor.execute(userMock, repositoryMock) } returns Response.success(
+            emptyList()
+        )
+
+        viewModel.getPullRequest(userMock,repositoryMock)
+
+        assertEquals(
+            viewModel.getPullRequestViewState().value,
+            PullRequestViewState.Empty
+        )
+    }
+
+    @Test
+    fun callsPullRequests_ErrorState() {
+        val errorbody = "".toResponseBody("aplication/json".toMediaType())
+
+        coEvery { interactor.execute(userMock, repositoryMock) } returns Response.error(
+            400, errorbody
+        )
+
+        viewModel.getPullRequest(userMock,repositoryMock)
+
+        assertEquals(
+            viewModel.getPullRequestViewState().value,
+            PullRequestViewState.Error
+        )
+    }
+
+    @Test
+    fun callsPullRequests_TimeOutSate(){
+        coEvery { interactor.execute(userMock,repositoryMock) } throws IOException()
+
+        viewModel.getPullRequest(userMock,repositoryMock)
+
+        assertEquals(
+            viewModel.getPullRequestViewState().value,
+            PullRequestViewState.ErrorTimeOut
+        )
     }
 }
